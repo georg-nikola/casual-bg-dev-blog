1530a35af165b0afedfc13f188af3afc3575a15f
georg-nikola <52797486+georg-nikola@users.noreply.github.com>
2022-08-07 20:00:38 +0300

    Initial template to be reworked

--- src/App.js
+++ src/App.js
@@ -3,6 +3,141 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container mb80">
+        <div className="page-timeline">
+          <div className="vtimeline-point">
+            <div className="vtimeline-icon">
+              <i className="fa fa-image" />
+            </div>
+            <div className="vtimeline-block">
+              <span className="vtimeline-date">June 25, 2020</span>
+              <div className="vtimeline-content">
+                <a href="#">
+                  <img
+                    src="https://via.placeholder.com/700x400"
+                    alt=""
+                    className="img-fluid mb20"
+                  />
+                </a>
+                <a href="#">
+                  <h3>Standard post title</h3>
+                </a>
+                <ul className="post-meta list-inline">
+                  <li className="list-inline-item">
+                    <i className="fa fa-user-circle-o" /> <a href="#">John Doe</a>
+                  </li>
+                  <li className="list-inline-item">
+                    <i className="fa fa-calendar-o" /> <a href="#">25 June 2020</a>
+                  </li>
+                  <li className="list-inline-item">
+                    <i className="fa fa-tags" /> <a href="#">Bootstrap4</a>
+                  </li>
+                </ul>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+                  in iaculis ex. Etiam volutpat laoreet urna. Morbi ut tortor nec
+                  nulla commodo malesuada sit amet vel lacus. Fusce eget efficitur
+                  libero. Morbi dapibus porta quam laoreet placerat.
+                </p>
+                <br />
+                <a href="#" className="btn btn-outline-secondary btn-sm">
+                  Read More
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="vtimeline-point">
+            <div className="vtimeline-icon">
+              <i className="fa fa-image" />
+            </div>
+            <div className="vtimeline-block">
+              <span className="vtimeline-date">June 25, 2020</span>
+              <div className="vtimeline-content">
+                <div className="embed-responsive embed-responsive-21by9 mb20">
+                  <iframe
+                    src="https://www.youtube.com/embed/htPYk6QxacQ?ecver=2"
+                    style={{
+                      position: "absolute",
+                      width: "100%",
+                      height: "100%",
+                      left: 0
+                    }}
+                    allowFullScreen=""
+                    width={640}
+                    height={360}
+                    frameBorder={0}
+                  />
+                </div>
+                <a href="#">
+                  <h3>Standard post title</h3>
+                </a>
+                <ul className="post-meta list-inline">
+                  <li className="list-inline-item">
+                    <i className="fa fa-user-circle-o" /> <a href="#">John Doe</a>
+                  </li>
+                  <li className="list-inline-item">
+                    <i className="fa fa-calendar-o" /> <a href="#">25 June 2020</a>
+                  </li>
+                  <li className="list-inline-item">
+                    <i className="fa fa-tags" /> <a href="#">Bootstrap4</a>
+                  </li>
+                </ul>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+                  in iaculis ex. Etiam volutpat laoreet urna. Morbi ut tortor nec
+                  nulla commodo malesuada sit amet vel lacus. Fusce eget efficitur
+                  libero. Morbi dapibus porta quam laoreet placerat.
+                </p>
+                <br />
+                <a href="#" className="btn btn-outline-secondary btn-sm">
+                  Read More
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="vtimeline-point">
+            <div className="vtimeline-icon">
+              <i className="fa fa-image" />
+            </div>
+            <div className="vtimeline-block">
+              <span className="vtimeline-date">June 25, 2020</span>
+              <div className="vtimeline-content">
+                <a href="#">
+                  <img
+                    src="https://via.placeholder.com/700x400"
+                    alt=""
+                    className="img-fluid mb20"
+                  />
+                </a>
+                <a href="#">
+                  <h3>Standard post title</h3>
+                </a>
+                <ul className="post-meta list-inline">
+                  <li className="list-inline-item">
+                    <i className="fa fa-user-circle-o" /> <a href="#">John Doe</a>
+                  </li>
+                  <li className="list-inline-item">
+                    <i className="fa fa-calendar-o" /> <a href="#">25 June 2020</a>
+                  </li>
+                  <li className="list-inline-item">
+                    <i className="fa fa-tags" /> <a href="#">Bootstrap4</a>
+                  </li>
+                </ul>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+                  in iaculis ex. Etiam volutpat laoreet urna. Morbi ut tortor nec
+                  nulla commodo malesuada sit amet vel lacus. Fusce eget efficitur
+                  libero. Morbi dapibus porta quam laoreet placerat.
+                </p>
+                <br />
+                <a href="#" className="btn btn-outline-secondary btn-sm">
+                  Read More
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
